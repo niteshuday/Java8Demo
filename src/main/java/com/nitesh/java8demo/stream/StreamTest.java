78a148c26c34ac1657bfbfd1fbd11416034050ad
Nitesh Kumar <31929070+niteshuday@users.noreply.github.com>
2024-08-04 14:53:20 +0530

    Adding more example

--- src/main/java/com/nitesh/java8demo/stream/StreamTest.java
+++ src/main/java/com/nitesh/java8demo/stream/StreamTest.java
@@ -4,12 +4,17 @@ import com.nitesh.java8demo.User;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 public class StreamTest {
     public static void main(String[] args) {
-        //********************************************************
+        //*****************Stream with lambda exp **************************
         //List<Integer> list= Arrays.asList(10,8,6,45,60,2,4,21,26,3);
         //Predicate the number is greater then 4
         //Predicate<Integer> predicate= num->num>4;
@@ -20,20 +25,34 @@ public class StreamTest {
         //StreamTest test= new StreamTest();
         //list.stream().filter(predicate).forEach(test::printNum2);
 
-        //***********************************************************
+        //**********************Stream Map or Flat Map*************************************
         //Where we can use map or flat map in stream
-        List<User> users= Stream.of(
-                new User("Shyam","12578546854",Arrays.asList("xyz@gmail.com","abc@gmail.com")),
-                new User("Raj","658455445545",Arrays.asList("raj1@gmail.com","raj5@gmail.com"))
-        ).collect(Collectors.toList());
+//        List<User> users= Stream.of(
+//                new User("Shyam","12578546854",Arrays.asList("xyz@gmail.com","abc@gmail.com")),
+//                new User("Raj","658455445545",Arrays.asList("raj1@gmail.com","raj5@gmail.com"))
+//        ).collect(Collectors.toList());
         //We are using Map where we did not have multiple values or array
         //Like if we want to print all the phone numbers or names, so we are using map.
-        List<String> phoneNumList=users.stream().map(User::getPhoneNumber).collect(Collectors.toList());
-        System.out.println(phoneNumList);
+//        List<String> phoneNumList=users.stream().map(User::getPhoneNumber).collect(Collectors.toList());
+//        System.out.println(phoneNumList);
         //We are using Flat Map where we  have multiple values or array
         //Like if we want to print all the emails without showing in array format
-        List<String> emails=users.stream().flatMap(user->user.getEmails().stream()).collect(Collectors.toList());
-        System.out.println(emails);
+//        List<String> emails=users.stream().flatMap(user->user.getEmails().stream()).collect(Collectors.toList());
+//        System.out.println(emails);
+
+        //***********************Stream grouping****************************
+        String inputStr="vikasprajapat";
+        //1.Changing input String into array--> inputStr.split("")
+        Map<String, Long> map = Arrays.stream(inputStr.split(""))
+                .collect(
+        //Importing a static Collectors in th top of the class which is having
+        // grouping by method and counting method
+        // Grouping by required 2 function which is mentioned below
+                        groupingBy(
+                                Function.identity(), counting()
+                        )
+                );
+        System.out.println(map);
 
     }
     public static void printNum(int i){
